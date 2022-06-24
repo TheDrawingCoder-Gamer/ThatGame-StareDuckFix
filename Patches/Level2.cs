3267dcf78421f6f29f1db93f18251d22754b8bc2
TheDrawingCoding-Gamer <bulbyvr@gmail.com>
2022-06-24 13:44:05 -0400

    Tweak Level 2 angles

--- Patches/Level2.cs
+++ Patches/Level2.cs
@@ -8,9 +8,11 @@ namespace StareDuckFix.Patches {
         private static void Postfix(Level02 __instance) {
             float distance = Vector3.Distance(__instance.player.position, __instance.duck.position);
             float alignedby = Vector3.Dot(__instance.playerTransform.forward, (__instance.duckTransform.position - __instance.player.position).normalized);
-            // map 0 to 10 to 0.5 to 0.95
-            // don't ask
-            float threshold = Mathf.Lerp(0f, 0.95f, Mathf.Clamp(0f, 10f, distance) / 10f);
+            // map 1.3 to 10 to -0.5 to 0.95
+            // 0.5 is ~120 deg, 0.95 is ~15 deg
+            // 1.3 is the distance when collison with the duck is activated
+            // 10 is just arbitrary LOL
+            float threshold = Mathf.Lerp(-0.5f, 0.95f, Mathf.Clamp(1.3f, 10f, distance) / 10f);
             if (alignedby > threshold) {
                 __instance.player.useGravity = false;
                 __instance.player.velocity = new Vector3(__instance.player.velocity.x, 0, __instance.player.velocity.z);
